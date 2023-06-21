97d0a7bfa90e707437872b20a6e5205e2452c05b
halitkalayci <halit.kalayci@ext.etiya.com>
2023-06-21 13:18:20 +0300

    login completed

--- src/starterProject/Application/Features/Auth/Commands/Login/LoginCommandResponse.cs
+++ src/starterProject/Application/Features/Auth/Commands/Login/LoginCommandResponse.cs
@@ -9,4 +9,21 @@ public class LoginCommandResponse : IResponse
     public AccessToken AccessToken { get; set; }
     public RefreshToken RefreshToken { get; set; }
     public AuthenticatorType RequiredAuthenticatorType { get; set; }
+
+    public LoginCommandHttpResponse ToHttpResponse()
+    {
+        return new LoginCommandHttpResponse()
+        {
+            AccessToken = this.AccessToken,
+            RequiredAuthenticatorType = this.RequiredAuthenticatorType
+        };
+    }
+
+    public class LoginCommandHttpResponse : IResponse
+    {
+        public AccessToken AccessToken { get; set; }
+        public AuthenticatorType RequiredAuthenticatorType { get; set; }
+
+    }
+
 }
--- src/starterProject/WebAPI/Controllers/AuthController.cs
+++ src/starterProject/WebAPI/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Features.Auth.Commands.Login;
 using Core.Application.Dtos;
+using Core.Security.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,8 +9,6 @@ namespace WebAPI.Controllers;
 [ApiController]
 public class AuthController : BaseController
 {
-
-
     [HttpPost]
     public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
     {
@@ -19,6 +18,14 @@ public class AuthController : BaseController
             IPAddress = getIpAddress()
         };
         var response = await Mediator.Send(command);
-        return Ok(response);
+        setRefreshTokenToCookie(response.RefreshToken);
+        return Ok(response.ToHttpResponse());
+    }
+
+    private string getRefreshTokenFromCookie => Request.Cookies["refreshToken"];
+    private void setRefreshTokenToCookie(RefreshToken refreshToken)
+    {
+        CookieOptions cookieOptions = new() { HttpOnly = true, Expires=DateTime.UtcNow.AddDays(7) };
+        Response.Cookies.Append(key:"refreshToken", refreshToken.Token, cookieOptions);
     }
 }
