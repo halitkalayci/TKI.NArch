6161c0b79ad7111f03644cc53494ec48327b8ba5
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-11 09:44:24 +0300

    cloudinary

---
+++ src/starterProject/Infrastructure/FileUpload/Services/Cloudinary/CloudinaryAdapter.cs
@@ -0,0 +1,16 @@
+﻿using CloudinaryDotNet;
+using Infrastructure.FileUpload.Adapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.FileUpload.Services.Cloudinary;
+public class CloudinaryAdapter : IFileUploadAdapter
+{
+    public Task<string> UploadImage(string fileBase64)
+    {
+        Account account = new Account("dusm8cdbj", "816141497288549", "HNgo1lyYqrN8uOv0VhkPTDtjYGE");
+    }
+}
