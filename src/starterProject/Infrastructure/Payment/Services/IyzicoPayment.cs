28e88ea3f02f378b7ca98e7d92fde20491d8b66a
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-19 09:51:14 +0300

    payment

--- src/starterProject/Infrastructure/Payment/Services/IyzicoPayment.cs
+++ src/starterProject/Infrastructure/Payment/Services/IyzicoPayment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Iyzipay;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,14 @@ namespace Infrastructure.Payment.Services
     // Dış kütüphane, IyziCo developerları tarafından yazılmış.
     public class IyzicoPayment
     {
+        public static Options GetOptions()
+        {
+            Options options = new Options();
+            options.ApiKey = "sandbox-Lv06ifEHYcTD4k319bE7wyzcEKxuD4f3";
+            options.SecretKey = "sandbox-iHJnmQt8930EOgmoD9yCoN0FhnSMDykk";
+            options.BaseUrl = "https://sandbox-api.iyzipay.com";
+            return options;
+        }
         public bool Pay(string creditCardNo, string cvc, DateTime expireTime)
         {
             return true;
