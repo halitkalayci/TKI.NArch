f2c012c6b102b86730f04bb2172e797c8b59ba12
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-19 10:29:04 +0300

    direct pay

---
+++ src/starterProject/WebAPI/Controllers/PaymentsController.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Payment.Adapters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class PaymentsController : BaseController
+{
+    private IPosServiceAdapter _posServiceAdapter;
+
+    [HttpPost]
+    public IActionResult Pay([FromBody] PaymentModel model)
+    {
+        _posServiceAdapter = new IyziCoPosServiceAdapter();
+        return Ok(_posServiceAdapter.Pay(model.CreditCardNo, model.CVC, model.ExpireTime));
+    }
+}
+
+public class PaymentModel
+{
+    public string CreditCardNo { get; set; }
+    public short CVC { get; set; }
+    public DateTime ExpireTime { get; set; }
+}
