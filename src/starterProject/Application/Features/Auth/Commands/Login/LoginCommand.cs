7d28b9845d30cc515d5721f879eac5c405e5fa90
halitkalayci <halit.kalayci@ext.etiya.com>
2023-08-02 11:52:04 +0300

    .

--- src/starterProject/Application/Features/Auth/Commands/Login/LoginCommand.cs
+++ src/starterProject/Application/Features/Auth/Commands/Login/LoginCommand.cs
@@ -3,15 +3,18 @@ using Application.Services.Auth;
 using Application.Services.Authenticator;
 using Application.Services.UserService;
 using Core.Application.Dtos;
+using Core.Mailing;
 using Core.Security.Entities;
 using Core.Security.Enums;
 using Core.Security.JWT;
 using MediatR;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Application.Features.Auth.Commands.Login;
 public class LoginCommand : IRequest<LoginCommandResponse>
@@ -25,16 +28,20 @@ public class LoginCommand : IRequest<LoginCommandResponse>
         private IUserService _userService;
         private AuthBusinessRules _authBusinessRules;
         private IAuthenticatorService _authenticatorService;
-        public LoginCommandHandler(IAuthService authService, IUserService userService, AuthBusinessRules authBusinessRules, IAuthenticatorService authenticatorService)
+        private IMailService _mailService;
+        public LoginCommandHandler(IAuthService authService, IUserService userService, AuthBusinessRules authBusinessRules, IAuthenticatorService authenticatorService, IMailService mailService)
         {
             _authService = authService;
             _userService = userService;
             _authBusinessRules = authBusinessRules;
             _authenticatorService = authenticatorService;
+            _mailService = mailService;
         }
 
         public async Task<LoginCommandResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
         {
+           
+
             User? userToLogin = await _userService.GetByEmail(request.UserForLoginDto.Email);
             
             await _authBusinessRules.UserMustExist(userToLogin);
@@ -58,6 +65,33 @@ public class LoginCommand : IRequest<LoginCommandResponse>
             RefreshToken refreshToken = await _authService.CreateRefreshToken(userToLogin, request.IPAddress);
 
             await _authService.AddRefreshToken(refreshToken);
+
+            //var text = System.IO.File.ReadAllText("wwwroot/templates/html/login.html");
+            //text = text.Replace("{{name}}", userToLogin.FirstName);
+            //text = text.Replace("{{Product Name}}", "TKI");
+            //text = text.Replace("{{username}}", userToLogin.Email);
+
+            //var emailList = new List<MailboxAddress>() { new(name: $"{userToLogin.FirstName} {userToLogin.LastName}", userToLogin.Email) };
+
+            //_mailService.SendMail(new Mail()
+            //{
+            //    ToList = emailList,
+            //    Subject = "TKI Rent A Car - Hoşgeldiniz",
+            //    HtmlBody = text
+            //});
+
+
+            //var word = System.IO.File.ReadAllText("wwwroot/templates/word/template2.xhtml");
+            //word = word.Replace("{{username}}", "Halit");
+            //word = word.Replace("{{lastname}}", "Kalaycı");
+            //word = word.Replace("{{email}}", "Halit@gmail.com");
+
+            //_mailService.SendMail(new Mail()
+            //{
+            //    ToList = emailList,
+            //    Subject = "TKI Rent A Car - Word",
+            //    HtmlBody = word
+            //});
 
             return new LoginCommandResponse { AccessToken=accessToken, RefreshToken=refreshToken, RequiredAuthenticatorType=userToLogin.AuthenticatorType};
         }
--- src/starterProject/WebAPI/Program.cs
+++ src/starterProject/WebAPI/Program.cs
@@ -77,6 +77,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseStaticFiles();
 
 app.ConfigureCustomExceptionMiddleware();
 
